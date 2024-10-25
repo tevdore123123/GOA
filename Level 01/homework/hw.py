db7e8dde409e657aed3dd9cfab7a9eea28baf852
tevdore123123 <nagervadzetevdore141@gmail.com>
2024-10-25 15:54:42 +0400

    first commit

---
+++ Level 01/homework/hw.py
@@ -0,0 +1,33 @@
+NAME = "TEDO"
+#name  არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობისმომნიჭებელი სიმბოლო
+# "tedo"  არის ცვლადისთვის  მნიშვნელობა
+
+SURNAME = "NAGERVADZE"
+
+print(NAME)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+name = "tedo" #ეს არის str (string) ტიპის  ცვლადი
+age = 11 # ეს არის int ( integer ) მთელი რიცხვი
+height = 156.3 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True  #True ან False
+is_ugly = False   #snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+surname = "nagervadze"
+print(name + " " + surname)
+
+ # #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+ # print(type(age))
+ # print(type(name))
+ # print(type(surname))
+ # print(type(height))
+ # print(type(knows_programing))
+
+print(name + " " + str(age))
